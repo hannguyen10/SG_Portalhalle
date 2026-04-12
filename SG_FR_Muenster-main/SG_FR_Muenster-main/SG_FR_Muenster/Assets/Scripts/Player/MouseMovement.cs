fc767ae291a826a33852697a9d6516828552b616
hannguyen10 <baohannguyen864@yahoo.de>
2026-04-12 20:06:12 +0200

    Update MouseMovement.cs

--- SG_FR_Muenster-main/SG_FR_Muenster-main/SG_FR_Muenster/Assets/Scripts/Player/MouseMovement.cs
+++ SG_FR_Muenster-main/SG_FR_Muenster-main/SG_FR_Muenster/Assets/Scripts/Player/MouseMovement.cs
@@ -12,8 +12,8 @@ public class MouseMovement : MonoBehaviour
     public Camera cam;
     private float xRotation = 0f;
 
-    public float xSensitivity = 0.4f;
-    public float ySensitivity = 0.4f;
+    public float xSensitivity = 2f;
+    public float ySensitivity = 2f;
 
     public void ProcessLook(Vector2 input)
     {
@@ -23,9 +23,7 @@ public class MouseMovement : MonoBehaviour
 
         //camera rotation for moving up and down
         xRotation -= (mouseY * Time.deltaTime) * ySensitivity;
-        // clamp xRotation between -80f & 80f
         xRotation = Mathf.Clamp(xRotation, -80f, 80f);
-        // apply to camera transform
         cam.transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
         // rotate player left and right
         transform.Rotate(Vector3.up * (mouseX * Time.deltaTime) * xSensitivity);
